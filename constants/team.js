84ed61e683194ce00d53ae59ce2302a021eaf36b
Jordan Teague <ejteague@gmail.com>
2022-02-07 19:54:34 -0500

    added pics

--- constants/team.js
+++ constants/team.js
@@ -1,10 +1,11 @@
-import punk from "../public/images/punk.png";
+import george from "../public/images/george.png";
+import jordan from "../public/images/jordan.png";
 
 export const team = [
   {
     name: "Jordan Teague",
     description: "Jordan is a super shadowy solidity engineer disguised as a lawyer.",
-    icon: punk.src,
+    icon: jordan.src,
     twitter: "https://twitter.com/jordanteague",
     discord: "https://discordapp.com/users/jordan_t#1507",
     linkedin: "https://www.linkedin.com/jordanteague"
@@ -12,7 +13,7 @@ export const team = [
   {
     name: "George Campbell",
     description: "George is a DAO ninja and was mining Ethereum before it was cool.",
-    icon: punk.src,
+    icon: george.src,
     twitter: "https://twitter.com/jordanteague",
     discord: "https://discordapp.com/users/jordan_t#1507",
     linkedin: "https://www.linkedin.com/jordanteague"
