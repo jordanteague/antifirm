efbacba7ebf39f3c0af713bb4eec257b341a2718
Jordan Teague <ejteague@gmail.com>
2022-03-01 14:38:09 -0500

    nits

--- constants/services.js
+++ constants/services.js
@@ -1,27 +1,32 @@
 export const services = [
   {
     name: "Web3 Startups",
-    description: "From forming a company to raising money to hiring employees, we've got you covered.",
-    icon: null
+    description:
+      "From forming a company to raising money to hiring employees, we've got you covered.",
+    icon: null,
+  },
+  {
+    name: "General Counsel",
+    description:
+      "We provide fractional GC services to projects that need ongoing support.",
+    icon: null,
   },
   {
     name: "DAO Structure",
-    description: "We'll help you figure out the best legal structure for your DAO.",
-    icon: null
+    description:
+      "We'll help you figure out the best legal structure for your DAO.",
+    icon: null,
   },
   {
     name: "NFT Sale Advice",
-    description: "We can advise on best practices for selling NFTs and how to protect your IP.",
-    icon: null
+    description:
+      "We can advise on best practices for selling NFTs and how to protect your IP.",
+    icon: null,
   },
   {
     name: "Token Sale Advice",
-    description: "We can help you navigate securities laws and exemptions that may impact your token sale.",
-    icon: null
+    description:
+      "We can help you navigate securities laws and exemptions that may impact your token sale.",
+    icon: null,
   },
-  {
-    name: "Smart Contracts",
-    description: "You got a security audit, but how about a legal audit? We can help.",
-    icon: null
-  }
-]
+];
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,35 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Box, Container, Wrap, WrapItem, Text, Heading, VStack, HStack, Button, IconButton, Link } from "@chakra-ui/react";
-import { SocialIcon } from 'react-social-icons';
-import { BsTwitter } from 'react-icons/bs';
-import { FaDiscord, FaLinkedinIn } from 'react-icons/fa';
+import {
+  Box,
+  Container,
+  Wrap,
+  WrapItem,
+  Text,
+  Heading,
+  VStack,
+  HStack,
+  Button,
+  IconButton,
+  Link,
+} from "@chakra-ui/react";
+import { SocialIcon } from "react-social-icons";
+import { BsTwitter } from "react-icons/bs";
+import { FaDiscord, FaLinkedinIn } from "react-icons/fa";
 import Layout from "../components/structure/Layout";
 import ContactForm from "../components/elements/ContactForm";
 import { services } from "../constants/services";
 import { team } from "../constants/team";
 
 export default function Home() {
-
-  console.log()
+  console.log();
 
   return (
     <Layout>
-
       <Box id="home-first-row" className="row">
         <Heading as="h1">We're a web3 native law firm. 🚀</Heading>
-        <Text>Work with lawyer-coders who actually understand your project.</Text>
+        <Text>
+          Work with lawyer-coders who actually understand your project.
+        </Text>
         <ContactForm label="Let's Talk" />
       </Box>
 
@@ -25,18 +37,18 @@ export default function Home() {
         <Box className="row-inner">
           <Heading as="h2">What We Do</Heading>
           <Wrap id="services">
-          {services.map((item, index) => (
-            <WrapItem
-              className="service"
-              key={index}
-              width={{sm: '100%', md: '48%', lg: '32%', xl: '19%'}}
-            >
-              <VStack>
-                <Heading as="h3">{item.name}</Heading>
-                <Text>{item.description}</Text>
-              </VStack>
-            </WrapItem>
-          ))}
+            {services.map((item, index) => (
+              <WrapItem
+                className="service"
+                key={index}
+                width={{ sm: "100%", md: "48%", lg: "32%", xl: "19%" }}
+              >
+                <VStack>
+                  <Heading as="h3">{item.name}</Heading>
+                  <Text>{item.description}</Text>
+                </VStack>
+              </WrapItem>
+            ))}
           </Wrap>
         </Box>
       </Box>
@@ -44,36 +56,43 @@ export default function Home() {
       <Box id="home-third-row" className="row">
         <Box className="row-inner">
           <Heading as="h2">Meet the Team</Heading>
-          <Wrap
-            id="team"
-          >
-          {team.map((item, index) => (
-            <WrapItem
-              className="teammate"
-              width={{sm: '100%', md: '48%', lg: '48%', xl: '48%'}}
-            >
-              <HStack alignItems="start">
-                <Box
-                  width={{sm: '100%', md: '48%', lg: '48%', xl: '48%'}}
-                >
-                  <img src={item.icon} width="200" height="33" alt="founder" className="team-pic" />
-                </Box>
-                <VStack className="team-description">
-                  <Heading as="h3">{item.name}</Heading>
-                  <Text>{item.description}</Text>
-                  <HStack>
-                    <Link href={item.twitter}><IconButton icon={<BsTwitter />} /></Link>
-                    <Link href={item.discord}><IconButton icon={<FaDiscord />} /></Link>
-                    <Link href={item.linkedin}><IconButton icon={<FaLinkedinIn />} /></Link>
-                  </HStack>
-                </VStack>
-              </HStack>
-            </WrapItem>
+          <Wrap id="team">
+            {team.map((item, index) => (
+              <WrapItem
+                className="teammate"
+                width={{ sm: "100%", md: "48%", lg: "48%", xl: "48%" }}
+              >
+                <HStack alignItems="start">
+                  <Box width={{ sm: "100%", md: "48%", lg: "48%", xl: "48%" }}>
+                    <img
+                      src={item.icon}
+                      width="200"
+                      height="33"
+                      alt="founder"
+                      className="team-pic"
+                    />
+                  </Box>
+                  <VStack className="team-description">
+                    <Heading as="h3">{item.name}</Heading>
+                    <Text>{item.description}</Text>
+                    <HStack>
+                      <Link href={item.twitter}>
+                        <IconButton icon={<BsTwitter />} />
+                      </Link>
+                      <Link href={item.discord}>
+                        <IconButton icon={<FaDiscord />} />
+                      </Link>
+                      <Link href={item.linkedin}>
+                        <IconButton icon={<FaLinkedinIn />} />
+                      </Link>
+                    </HStack>
+                  </VStack>
+                </HStack>
+              </WrapItem>
             ))}
           </Wrap>
         </Box>
       </Box>
-
     </Layout>
   );
 }
